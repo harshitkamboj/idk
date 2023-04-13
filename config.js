0d7d3c9f50894407aee06cbd89523c69f43167b8
harshitkamboj <71609676+harshitkamboj@users.noreply.github.com>
2023-04-13 15:33:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,9 +25,9 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "lavalink-replit.mrrobot61.repl.co", //- The host name or IP of the lavalink server.
+			host: "lavalink.lexnet.cc", //- The host name or IP of the lavalink server.
 			port: 443, // The port that lavalink is listening to. This must be a number!
-			password: "MrRobot123!", //- The password of the lavalink server.
+			password: "lexn3tl@val!nk", //- The password of the lavalink server.
 			retryAmount: 500, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: true, //- Can be either true or false. Only use true if ssl is enabled!
